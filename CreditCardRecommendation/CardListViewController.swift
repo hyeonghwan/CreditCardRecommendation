f7826e89c5684807ff7b34f6fc4624d47af1e1ea
HyeongHwan <gudghks56@gmail.com>
2022-02-23 22:57:04 +0900

    firebase realtimeDatabase

--- CreditCardRecommendation/CardListViewController.swift
+++ CreditCardRecommendation/CardListViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
 class CardListViewController: UITableViewController {
 
@@ -16,26 +17,47 @@ class CardListViewController: UITableViewController {
         let component = UINib(nibName: "CardListCell", bundle: nil)
         tableView.register(component, forCellReuseIdentifier: "CardCell")
        
-        guard
-            let jsonData = load(),
-            let cardData = try? JSONDecoder().decode([String : CardData].self, from: jsonData)
-        else {
-            return }
-        cardListData = cardData
-        debugPrint(cardListData.keys)
-
+//        guard let jsonData = load() else { return }
+//        cardListData = jsonData
+//        debugPrint(cardListData.keys)
+        firebaseGetData()
+       
+        debugPrint("박형ㄴ환 : \(cardListData)")
        
     }
-    fileprivate func load() -> Data? {
+    fileprivate func load() -> [String : CardData]? {
         let fileName: String = "credit-card-list"
         let extensionType: String = "json"
         guard let fileLocation = Bundle.main.url(forResource: fileName, withExtension: extensionType) else { return nil }
         do{
             let data = try Data(contentsOf: fileLocation)
-            return data
+            let cardData = try? JSONDecoder().decode([String : CardData].self, from: data)
+            return cardData
         }catch{
             return nil
         }
+    }
+    fileprivate func firebaseGetData(){
+        var ref: DatabaseReference!
+
+        ref = Database.database().reference()
+       
+            ref.child("/").getData(completion:  { [weak self] error, snapshot in
+              guard error == nil else {
+                print(error!.localizedDescription)
+                return;
+              }
+                do{
+                    let jsonData = try JSONSerialization.data(withJSONObject: snapshot.value!)
+                    let data = try JSONDecoder().decode([String : CardData].self, from: jsonData)
+                    guard let self = self else {return}
+                    self.cardListData = data
+                    self.tableView.reloadData()
+                }catch{
+                    debugPrint("catch - nil")
+                }
+            });
+        
     }
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         guard let vc = storyboard?.instantiateViewController(withIdentifier: "ViewController") as? ViewController else { return }
