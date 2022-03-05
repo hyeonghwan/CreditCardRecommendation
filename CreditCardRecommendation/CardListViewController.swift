d5fce8aa7c40e1b6c6da905067fcdc6d903ec1c6
HyeongHwan <gudghks56@gmail.com>
2022-03-05 14:09:07 +0900

    table correction

--- CreditCardRecommendation/CardListViewController.swift
+++ CreditCardRecommendation/CardListViewController.swift
@@ -7,24 +7,24 @@
 
 import UIKit
 import FirebaseDatabase
-
+import SwiftUI
 class CardListViewController: UITableViewController {
+ 
 
     var cardListData: [String : CardData] = [:]
+    var cardList: [CardData] = []
+    var ref: DatabaseReference!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let component = UINib(nibName: "CardListCell", bundle: nil)
         tableView.register(component, forCellReuseIdentifier: "CardCell")
-       
-//        guard let jsonData = load() else { return }
-//        cardListData = jsonData
-//        debugPrint(cardListData.keys)
+
         firebaseGetData()
-       
-        debugPrint("박형ㄴ환 : \(cardListData)")
-       
-    }
+    } //viewDidLoad
+    
+    //Bundle.main.url 경로에서 json 파일 불러오기
     fileprivate func load() -> [String : CardData]? {
         let fileName: String = "credit-card-list"
         let extensionType: String = "json"
@@ -37,11 +37,9 @@ class CardListViewController: UITableViewController {
             return nil
         }
     }
+    //파이어베이스 realtimedataBase 이용
     fileprivate func firebaseGetData(){
-        var ref: DatabaseReference!
-
         ref = Database.database().reference()
-       
             ref.child("/").getData(completion:  { [weak self] error, snapshot in
               guard error == nil else {
                 print(error!.localizedDescription)
@@ -51,32 +49,43 @@ class CardListViewController: UITableViewController {
                     let jsonData = try JSONSerialization.data(withJSONObject: snapshot.value!)
                     let data = try JSONDecoder().decode([String : CardData].self, from: jsonData)
                     guard let self = self else {return}
-                    self.cardListData = data
-                    self.tableView.reloadData()
+                    self.cardList = data.values.sorted{$0.rank < $1.rank}
+                    DispatchQueue.main.async {
+                        self.tableView.reloadData()
+                    }
                 }catch{
                     debugPrint("catch - nil")
                 }
             });
-        
     }
+    
+    //Cell에 있는 버튼 클릭시
+    @objc fileprivate func arrowButtonClicked(_ sender: ArrowButton) {
+        debugPrint("arrowButtonClicked - called")
+        guard let vc = storyboard?.instantiateViewController(withIdentifier: "ViewController") as? ViewController else { return }
+        guard let index = sender.indexPath?.row else {return}
+        vc.cardData = cardList[index]
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         guard let vc = storyboard?.instantiateViewController(withIdentifier: "ViewController") as? ViewController else { return }
+        vc.cardData = cardList[indexPath.row]
+        let id = cardList[indexPath.row].id
+        ref.child("Item\(id)/isSelected").setValue(true)
         self.navigationController?.pushViewController(vc, animated: true)
-        
     }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return cardListData.count
+        return cardList.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = self.tableView.dequeueReusableCell(withIdentifier: "CardCell", for: indexPath) as? CardCell else {return UITableViewCell() }
-        
-        cell.cardCellData = cardListData["Item\(indexPath.row)"]
-
-        
-        
+        cell.cardCellData = cardList[indexPath.row]
+        cell.arrow.indexPath = indexPath
+        cell.arrow.addTarget(self, action: #selector(arrowButtonClicked(_:)), for: .touchUpInside)
         return cell
     }
-    
-    
 }
+
