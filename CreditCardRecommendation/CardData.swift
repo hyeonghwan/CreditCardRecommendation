6b7b9d525e7bc0a8252c36357105b18e810b55c2
HyeongHwan <gudghks56@gmail.com>
2022-03-06 19:39:40 +0900

    add asDictionary (convert from struct to JSON)

--- CreditCardRecommendation/CardData.swift
+++ CreditCardRecommendation/CardData.swift
@@ -7,20 +7,33 @@
 
 import Foundation
 
-struct CardData: Codable{
-    var cardImageURL: String
+struct CardData: Convertable{
     var id: Int
     var rank: Int
     var name: String
+    var cardImageURL: String
     var promotionDetail: PromotionDetail
+    var isSelected: Bool?
+    
+    
+    // struct를 Dictionary로 변환 해주는 변수
+    var asDictionary : [String:Any] {
+       let mirror = Mirror(reflecting: self)
+       let dict = Dictionary(uniqueKeysWithValues: mirror.children.lazy.map({ (label:String?, value:Any) -> (String, Any)? in
+         guard let label = label else { return nil }
+         return (label, value)
+       }).compactMap { $0 })
+       return dict
+     }
+    
 }
 struct PromotionDetail: Codable{
     var companyName: String
-    var amount: Int
     var period: String
-    var benefitDate: String
-    var benefitDetail: String
-    var benefitCondition: String
+    var amount: Int
     var condition: String
+    var benefitCondition: String
+    var benefitDetail: String
+    var benefitDate: String
 }
 
