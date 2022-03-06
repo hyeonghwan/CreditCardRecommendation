2f78bbff7af24f3960eec717324887a4c5acef6b
HyeongHwan <gudghks56@gmail.com>
2022-03-06 19:42:54 +0900

    add lottie, animationView , DeatailView

--- CreditCardRecommendation/ViewController.swift
+++ CreditCardRecommendation/ViewController.swift
@@ -6,15 +6,63 @@
 //
 
 import UIKit
-
+import SwiftUI
+import Lottie
+import FirebaseDatabase
 class ViewController: UIViewController {
 
+    @IBOutlet weak var period: UILabel!
+    @IBOutlet weak var condition: UILabel!
+    @IBOutlet weak var benefitCondition: UILabel!
+    @IBOutlet weak var benefitDetail: UILabel!
+    @IBOutlet weak var benefitDate: UILabel!
+    
+    @IBOutlet weak var animationView: AnimationView!
+    var cardData: CardData?
+    var ref: DatabaseReference!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-    
+        ref = Database.database().reference()
+        labelTextConfiguration()
+        let lottieView = AnimationView(name: "money")
+        animationView.addSubview(lottieView)
+        lottieView.frame = animationView.bounds
+        lottieView.contentMode = .scaleAspectFit
+        lottieView.loopMode = .loop
+        lottieView.play(completion: nil)
     }
-
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        guard let cardData = cardData else {return}
+        ref.child("Item\(cardData.id)/isSelected").setValue(nil)
+        debugPrint("viewDidDisappear - called")
+    }
+    
+    fileprivate func labelTextConfiguration() {
+        period.text = cardData?.promotionDetail.period
+        condition.text = cardData?.promotionDetail.condition
+        benefitDetail.text = cardData?.promotionDetail.benefitDetail
+        benefitCondition.text = cardData?.promotionDetail.benefitCondition
+        benefitDate.text = cardData?.promotionDetail.benefitDate
+    }
+    
 
 }
+#if canImport(SwiftUI) && DEBUG
+@available(iOS 13, *)
+struct ViewControllerRepresentable: UIViewControllerRepresentable {
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+        
+    }
+    func makeUIViewController(context: Context) -> UIViewController {
+        ViewController()
+    }
+    struct ViewController_Previews: PreviewProvider {
+        static var previews: some View{
+            ViewControllerRepresentable()
+        }
+    }
+}
+#endif
 
